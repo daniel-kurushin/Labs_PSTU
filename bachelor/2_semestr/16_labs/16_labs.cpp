d55fbbe22d47548c6076a73e0e9543ca56772a58
Honepa <cot1884@gmail.com>
2020-04-16 00:12:14 +0500

    lab num 16

--- bachelor/2_semestr/16_labs/16_labs.cpp
+++ bachelor/2_semestr/16_labs/16_labs.cpp
@@ -1,7 +1,17 @@
 //Духанин Алексей. 16 лаб.
 #include <iostream>
 using namespace std;
-
+int * create_arr(int n)
+{
+    int *arr = new int[n];
+    for(int counter = 0; counter < n; counter++)
+    {
+        arr[counter] = counter;
+        cout << counter << '\t';
+    }
+    cout << endl;
+    return arr;
+}
 int search_murrr(string words, string subwords)
 {
     int words_len, subwords_len, pos;
@@ -45,10 +55,46 @@ int search_murrr(string words, string subwords)
     return 2;
 }
 
+int search_inter(int * arr, int n, int num)
+{
+    int left, right, index;
+    left = index = 0;
+    right = n;
+    while((left <= right))
+    {
+        index = left + ((num - arr[left]) * (right - left)) / (arr[right] - arr[left]);
+        if(arr[index] < num)
+        { 
+            left = index + 1;
+        }
+        else if (arr[index] > num)
+        { 
+            right = index + 1;
+        }
+        else
+        {
+            break;
+        }
+    }
+    if (arr[left] == num)
+    {
+        return left;
+    }
+    else if (arr[right] == num)
+    {
+        return left;
+    }
+    return -2;
+}
+
 int main(int argc, char * argv[])
 {
+    int * arr;
+    int n = 10;
+    arr = create_arr(n);
     int index_boer_mur = search_murrr(argv[1], argv[2]);
     index_boer_mur  == 2 ? cout << "Not search" << endl : cout << "Боер - Мур: " << index_boer_mur << endl;
-    
+    int index_inter = search_inter(arr, n, atoi(argv[3]));
+    index_inter == -2 ? cout << "Not search" << endl : cout << "Интерпол.: " << index_inter << endl;
     return 0;
 }
