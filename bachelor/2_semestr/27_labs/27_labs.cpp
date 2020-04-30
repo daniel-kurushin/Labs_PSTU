85154f701784b23ef9016fbfd9a7712de2d5c528
Honepa <cot1884@gmail.com>
2020-05-01 03:42:05 +0500

    lab num 27

--- bachelor/2_semestr/27_labs/27_labs.cpp
+++ bachelor/2_semestr/27_labs/27_labs.cpp
@@ -60,10 +60,57 @@ istream& operator>>(istream& in, Pair &in_pair)
     return in;
 }
 
+fstream& operator >>(fstream& fin, Pair &in_pair)
+{
+    fin >> in_pair.first;
+    fin >> in_pair.second;
+    return fin;
+}
+
+fstream& operator << (fstream& fout, Pair &in_pair)
+{
+    fout << in_pair.first << " : " << in_pair.second << '\n';
+    return fout;
+}
+
+int make_file(const char* file_name)
+{
+    fstream stream(file_name, ios::out|ios::trunc);
+    if(!stream)
+    {
+        return 2;
+    }
+    Pair one;
+    int num;
+    cin >> num;
+    for(int counter = 0; counter < num; counter++)
+    {
+        cin >> one;
+        stream << one << '\n';
+    }
+    stream.close();
+    return num;
+}
+
 int main(int argc, char* argv[])
 {
     Pair x;
-    cin >> x;
-    cout << x;
+    int state, eror = 1;
+    char file_name[30];
+    while(state)
+    {
+        cout << "file? ";
+        cin >> file_name;
+        eror = make_file(file_name);
+        if(eror == 2)
+        {
+            cout << "cant make file, exit - 0, cont - 1 ";
+        }
+        else
+        {
+            cout << "exit - 0, cont - 1 ";
+        }
+        cin >> state;
+    }
     return 0;
 }
