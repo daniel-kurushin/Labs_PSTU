aebf0e3bda0375d4a175df21489a7616fcd4f3f8
Honepa <cot1884@gmail.com>
2020-05-07 17:51:12 +0500

    lab num 26

--- bachelor/2_semestr/26_labs/26_labs.cpp
+++ bachelor/2_semestr/26_labs/26_labs.cpp
@@ -1,32 +1,44 @@
 #include <iostream>
-#include <error.h>
-#include <List.h>
 
 using namespace std;
 
 class Error
 {
-    
+    string str;
+    public:
+    Error(string s)
+    {
+        str = s;
+    }
+    void what()
+    {
+        cout << str << endl;
+    }
 };
 
 class List
 {
     private:
+    int count = 0;
     struct list
     {
         int data;
-        int count = 0;
-        struct list * head;
-    }
+        struct list * next;
+    };
     list * head;
     public:
     List ()
     {
+        count = 0;
         head = NULL;
     }
     void add(int in_data)
     {
-        
+        count++;
+        list * tmp = new list;
+        tmp -> data = in_data;
+        tmp -> next = head;
+        head = tmp;
     }
     void print()
     {
@@ -40,25 +52,49 @@ class List
     }
     int & operator[](int index)
     {
-        
+        if (index >= count) 
+            throw Error("over index");
+        list * tmp = head;
+        for(int counter = 0; counter < index; counter++)
+        {
+            tmp = tmp -> next;
+        }
+        return tmp -> data;
     }
     int & operator() ()
     {
+        if (count == 0) throw Error("list is empty!");
         return count;
     }
     int& operator*(int num)
     {
-    
+        if(num < 0) throw Error("input num");
+        list * tmp = head;
+        while(tmp != NULL)
+        {
+            tmp -> data = tmp -> data * num;
+            tmp = tmp -> next;
+        }
+        return tmp -> data;
     }
 };
 
-int main(int argc, char argv)
+int main(int argc, char* argv[])
 {
+    List tree;
     try
     {
-        
+        for(int i = 0; i < 5; i++)
+        {
+            tree.add(i);
+        }
+        tree * 5;
+        tree.print();
+        cout << tree[7] << endl;
+        tree * -1;
+        tree.print();
     }
-    catch(Error &e)
+    catch(Error e)
         {
             e.what();
         }
