483a5bf7c88156322b319e1b9ae2d76c797dcad2
Honepa <cot1884@gmail.com>
2020-05-16 01:26:02 +0500

    working buttons

--- bachelor/2_semestr/33_labs/33_lab.cpp
+++ bachelor/2_semestr/33_labs/33_lab.cpp
@@ -7,6 +7,9 @@
 
 using namespace std;
 
+int all_sec, conver_sec;
+int all_hours = 0;
+int all_min = 0;
 class Time
 {
     private:
@@ -18,17 +21,86 @@ class Time
     void set_hours(string in_time)
     {
         hours = atoi(in_time.c_str());
-        cout << hours << endl;
+        cout << hours << " hours" << endl;
     }
     void set_min(string in_time)
     {
         min = atoi(in_time.c_str());
-        cout << min << endl;
+        cout << min << " min" << endl;
     }
     void set_sec(string in_time)
     {
         sec = atoi(in_time.c_str());
-        cout << sec << endl;
+        cout << sec << " sec" << endl;
+    }
+    void minus(Time &in_time_two)
+    {
+        int all_sec_one = 0; 
+        int all_sec_two = 0;
+        all_sec = 0;
+        all_min = 0;
+        all_hours = 0;
+        all_sec_one = hours * 3600 + min * 60 + sec;
+        all_sec_two = in_time_two.hours * 3600 + in_time_two.min * 60 + in_time_two.sec;
+        all_sec = all_sec_one - all_sec_two;
+        
+        while(all_sec >= 60)
+        {
+            all_min++;
+            all_sec = all_sec - 60;
+        }
+        while(all_min >= 60)
+        {
+            all_hours++;
+            all_min = all_min - 60;
+        }
+        cout << all_hours << " hours " << all_min << " min " << all_sec << " sec " << endl;
+    }
+    void plus(Time &in_time_two)
+    {
+        int all_sec_one = 0; 
+        int all_sec_two = 0;
+        all_sec = 0;
+        all_min = 0;
+        all_hours = 0;
+        all_sec_one = hours * 3600 + min * 60 + sec;
+        all_sec_two = in_time_two.hours * 3600 + in_time_two.min * 60 + in_time_two.sec;
+        all_sec = all_sec_one + all_sec_two;
+        
+        while(all_sec >= 60)
+        {
+            all_min++;
+            all_sec = all_sec - 60;
+        }
+        while(all_min >= 60)
+        {
+            all_hours++;
+            all_min = all_min - 60;
+        }
+        cout << all_hours << " hours " << all_min << " min " << all_sec << " sec " << endl;
+    }
+    void time_inter(Time &in_time_two)
+    {
+        int all_sec_one = 0; 
+        int all_sec_two = 0;
+        all_sec = 0;
+        all_min = 0;
+        all_hours = 0;
+        all_sec_one = hours * 3600 + min * 60 + sec;
+        all_sec_two = in_time_two.hours * 3600 + in_time_two.min * 60 + in_time_two.sec;
+        all_sec = all_sec_two - all_sec_one;
+        conver_sec = all_sec;
+        while(all_sec >= 60)
+        {
+            all_min++;
+            all_sec = all_sec - 60;
+        }
+        while(all_min >= 60)
+        {
+            all_hours++;
+            all_min = all_min - 60;
+        }
+        cout << all_hours << " hours " << all_min << " min " << all_sec << " sec " << endl;
     }
 };
 
@@ -72,6 +144,40 @@ static void chek_sec2(GtkWidget *widget, gpointer data)
 {
     in_time = gtk_entry_get_text(GTK_ENTRY(entry_time2));
     time2.set_sec(in_time);
+}
+
+static void chek_minus(GtkWidget *widget, gpointer data)
+{
+    time1.minus(time2);
+}
+
+static void chek_plus(GtkWidget *widget, gpointer data)
+{
+    time1.plus(time2);
+}
+
+static void chek_inter(GtkWidget *widget, gpointer data)
+{
+    time1.time_inter(time2);
+}
+
+static void chek_conver(GtkWidget *widget, gpointer data)
+{
+    cout << conver_sec << " sec ";
+    int conver_min   = 0;
+    int conver_hours = 0;
+    while(conver_sec >= 60)
+    {
+        conver_min++;
+        conver_sec = conver_sec - 60;
+    }
+    cout << conver_min << " min ";
+    while(conver_min >= 60)
+    {
+        conver_hours++;
+        conver_min = conver_min - 60;
+    }
+    cout << conver_hours << " hours " << endl;
 }
 
 static void activate (GtkApplication* app, gpointer user_data)
@@ -128,19 +234,19 @@ static void activate (GtkApplication* app, gpointer user_data)
     gtk_grid_attach(GTK_GRID(grid), button, 0, 7, 1, 1);
     
     button = gtk_button_new_with_label("-");
-    g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
+    g_signal_connect_swapped(button, "clicked", G_CALLBACK(chek_minus), window);
     gtk_grid_attach(GTK_GRID(grid), button, 0, 8, 1, 1);
     
     button = gtk_button_new_with_label("+");
-    g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
+    g_signal_connect_swapped(button, "clicked", G_CALLBACK(chek_plus), window);
     gtk_grid_attach(GTK_GRID(grid), button, 1, 8, 1, 1);
     
     button = gtk_button_new_with_label("Интер");
-    g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
+    g_signal_connect_swapped(button, "clicked", G_CALLBACK(chek_inter), window);
     gtk_grid_attach(GTK_GRID(grid), button, 0, 9, 1, 1);
     
-    button = gtk_button_new_with_label("Пред");
-    g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
+    button = gtk_button_new_with_label("Преобр");
+    g_signal_connect_swapped(button, "clicked", G_CALLBACK(chek_conver), window);
     gtk_grid_attach(GTK_GRID(grid), button, 1, 9, 1, 1);
     
     gchar *str = "Ответ:";
