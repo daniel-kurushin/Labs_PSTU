3ee62e9070353e6ddd02d2a8af83b70bf85a3948
Honepa <cot1884@gmail.com>
2021-04-29 15:06:34 +0500

    4 semestr

---
+++ bachelor/4_semestr/dis_math/1_lab/1_lab.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 29 11:50:37 2021
+
+@author: honepa
+"""
+from arr import x
+import os
+from graphviz import Source
+import numpy as np
+
+temp= "digraph G {"
+incent_str = str()
+count = int()
+for i in range(len(x)):
+    for j in range(len(x)):
+        if(x[i][j]):
+            #print(i)
+            #print(j)
+            temp += " " + str(i + 1) + " -> " + str(j + 1) + " "
+            incent_str += "|"+ str(i + 1) + "-" + str(j + 1) + "|"
+            count += 1
+
+temp += "}"
+
+incent_arr = np.zeros((len(x), count), dtype=int)
+
+counter = 0
+
+for i in range(len(x)):
+    for j in range(len(x)):
+        if(x[i][j]):
+            incent_arr[i][counter] = 1
+            incent_arr[j][counter] = 1
+            counter += 1
+print("Матрица инцидентности:")
+print(" " + incent_str)
+
+for i in range(len(x)):
+    print(i + 1, end='')
+    for j in range(count):
+        print("  " + str(incent_arr[i][j]) + "  ", end='')
+    print('\n')
+s = Source(temp, filename="graph.gv", format="png")
+s.view()
+
+#os.system("xdot -f circo graph.gv")
